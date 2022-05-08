c5e66590d4a1dc091dd2c5516ea0de725eeac283
Fadel <fadel.g.heji@gmail.com>
2022-05-07 21:46:43 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+let body = document.body
+function submitData (userName,userEmail){
+   return( fetch('http://localhost:3000/users', {
+         method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({name : userName, email : userEmail}),
+  })
+    .then(resp => resp.json())
+    .then((json) => body.append(json.id))
+    .catch((error) => {
+        alert("You got an oopsy")
+        body.append(error.message)
+    }))
+}
